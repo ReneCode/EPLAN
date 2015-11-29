ecee355b18d3a114fad2589d774e0628368dbf3a
HarryMiller <HarryMillerMail@googlemail.com>
2015-11-29 15:17:33 +0100

    import data

---
+++ api/import.js
@@ -0,0 +1,49 @@
+var wagner = require('wagner-core');
+var _ = require('underscore');
+
+require('./schema/models.js')(wagner);
+
+PartsModel = wagner.invoke(function(Part) {
+	return Part;
+})
+
+var fileData = require('../try/parts.json');
+
+/*
+var p1 = {partnr:"xya"};
+var p2 = {partnr:"xsdf"};
+PartsModel.create([p1,p2], function(err, data) {
+	if (err) {
+		console.log("Error:", err);
+	}
+	console.log(data);
+
+	closeConnection();
+
+});
+*/
+
+
+// import all parts as one array
+PartsModel.create(fileData.parts, function(err, data) {
+	if (err) {
+		console.log("Error:", err);
+	}
+	console.log(data);
+
+	closeConnection();
+
+});
+
+
+
+function closeConnection() {
+	wagner.invoke(function(db) {
+		db.connection.close(function() {
+			console.log("fnished");
+		});
+	});
+
+}
+
+
