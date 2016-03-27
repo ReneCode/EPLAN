222795663a9b97ca83968a5cd7709d86f14201ca
HarryMiller <HarryMillerMail@googlemail.com>
2016-03-27 12:35:35 +0200

    durations

--- api/processUtility.js
+++ api/processUtility.js
@@ -2,15 +2,13 @@
 
 module.exports = (function() {
 
-	function addMinutes(date, minutes) {
+	function addMinute(date, minutes) {
 		return new Date(date.getTime() + minutes*60000);
 	}
 
 
 	function newDuration(process) {
-		return { id:process.id, 
-						 process_name: process.process_name, 
-						 start_at: process.start_at,
+		return { start_at: process.start_at,
 						 end_at: process.end_at,
 						};
 	}
@@ -31,19 +29,59 @@ module.exports = (function() {
 		} else {
 			return d1;
 		}
-
 	}
+
+	function minDate(d1, d2) {
+		if (d1 < d2) {
+			return d1;
+		} else {
+			return d2;
+		}
+	}
+
 
 	function calcDuration(processList) {
 		// sort by process_name
 		processList.sort( function(p1, p2) {
-			
+			if (p1.process_name < p2.process_name) {
+				return -1;
+			}
+			if (p1.process_name > p2.process_name) {
+				return 1;
+			}
+			return 0;
 		});
 
+		var ids = [];
+		processList.forEach( function(p) {
+			if (ids.indexOf(p.id) < 0) {
+				ids.push(p.id);
+			}
+		});
 
+		var durations = [];
+		ids.forEach( function(id) {
+			var listOfOneProcess = processList.filter( function(p) {
+				return p.id === id;
+			});
+			var sumDuration = calcSumOfDuration(listOfOneProcess);
+			durations.push( {
+				id: listOfOneProcess[0].id,
+				process_name: listOfOneProcess[0].process_name,
+				start_at: sumDuration.start_at,
+				end_at: sumDuration.end_at,
+				duration: sumDuration.duration,
+			});
+		});
+
+		return durations;
+	}
+
+
+	function calcSumOfDuration(processList) {
 		// add a end_at property
 		processList.forEach( function(p) {
-			p.end_at = addMinutes(p.start_at, p.duration);
+			p.end_at = addMinute(p.start_at, p.duration);
 		});
 
 		// sort by start_at
@@ -76,12 +114,33 @@ module.exports = (function() {
 			d.duration = Math.round( (d.end_at - d.start_at) / 60000 );
 		});
 
-		return durations;
+		var sumDuration = 0;
+		var minStartAt, maxEndAt;
+		durations.forEach(function(d) {
+			sumDuration += d.duration;
+			if (!minStartAt) {
+					minStartAt = d.start_at;	
+			} else {
+					minStartAt = minDate(minStartAt, d.start_at);
+			}
+			if (!maxEndAt) {
+				maxEndAt = d.end_at;
+			} else {
+				maxEndAt = maxDate(maxEndAt, d.end_at);
+			}
+		});
+		return { 
+			duration: sumDuration,
+			start_at: minStartAt,
+			end_at: maxEndAt
+		};
 	}
 	
 
 	return {
-		calcDuration: calcDuration
+		calcDuration: calcDuration,
+		calcSumOfDuration: calcSumOfDuration,
+		addMinute: addMinute
 	};
 
 })();
--- api/test/processUtility_test.js
+++ api/test/processUtility_test.js
@@ -9,7 +9,7 @@ describe('processDuration', function() {
 		it ('overlap', function() {
 			var oIn = [ 
 			{ id:4711, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 15), duration:14 },
-			{ id:4711, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 10), duration:6 },
+			{ id:4711, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 10), duration:7 },
 			];
 
 			var oOut = processUtility.calcDuration(oIn);
@@ -17,33 +17,43 @@ describe('processDuration', function() {
 			assert.equal(oOut[0].id, 4711);
 			assert.equal(oOut[0].process_name, 'javaw');
 			assert.equal(oOut[0].duration, 19);
+			assert.deepEqual(oOut[0].start_at, new Date(2016, 2, 2, 6, 10));
+			assert.deepEqual(oOut[0].end_at, new Date(2016, 2, 2, 6, 29));
 		});
 
 		it ('not overlap', function() {
+
 			var oIn = [ 
 			{ id:4711, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 25), duration:14 },
 			{ id:4711, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 10), duration:6 },
 			];
 
 			var oOut = processUtility.calcDuration(oIn);
-			assert.equal(oOut.length, 2);
+			assert.equal(oOut.length, 1);
 			assert.equal(oOut[0].id, 4711);
-			assert.equal(oOut[1].id, 4711);
-			assert.equal(oOut[0].duration, 6);
-			assert.equal(oOut[1].duration, 14);
+			assert.equal(oOut[0].duration, 20);
+			assert.deepEqual(oOut[0].start_at, new Date(2016, 2, 2, 6, 10));
+			assert.deepEqual(oOut[0].end_at, new Date(2016, 2, 2, 6, 39));
 		});
 
-		xit ('different processes - overlap', function() {
+		it ('different processes - overlap', function() {
 			var oIn = [ 
 			{ id:4711, process_name: 'cmd', start_at: new Date(2016, 2, 2, 6, 15), duration:14 },
-			{ id:4712, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 10), duration:6 },
+			{ id:4712, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 10), duration:7 },
+			{ id:4712, process_name: 'javaw', start_at: new Date(2016, 2, 2, 6, 15), duration:10 },
+			{ id:4713, process_name: 'abc', start_at: new Date(2016, 2, 2, 6, 10), duration:14 }
 			];
 
 			var oOut = processUtility.calcDuration(oIn);
-			assert.equal(oOut.length, 2);
-			assert.equal(oOut[0].id, 4711);
-			assert.equal(oOut[0].process_name, 'javaw');
-			assert.equal(oOut[0].duration, 19);
+			assert.equal(oOut.length, 3);
+			assert.equal(oOut[0].id, 4713);
+			assert.equal(oOut[0].process_name, 'abc');
+			assert.equal(oOut[0].duration, 14);
+			assert.equal(oOut[2].id, 4712);
+			assert.equal(oOut[2].duration, 15);
+			assert.deepEqual(oOut[2].start_at, new Date(2016, 2, 2, 6, 10));
+			assert.deepEqual(oOut[2].end_at, new Date(2016, 2, 2, 6, 25));
+
 		});
 
 	});
