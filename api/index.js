a4150b2560aadaa507111d6e6f688f3f1fb5d45b
HarryMiller <HarryMillerMail@googlemail.com>
2016-07-02 16:17:47 +0200

    repair index.js  server init

--- api/index.js
+++ api/index.js
@@ -5,8 +5,6 @@
 
 var server = require('./server.js');
 
-var app = server();
-
 /*
 var http = require('http');
 	
@@ -14,7 +12,7 @@ http.createServer(app).listen(port);
 */
 
 var port = 64010;
-app().listen(port, function() {
+server().listen(port, function() {
 	console.log('REST Server is listing on Port', port);
 });
 
